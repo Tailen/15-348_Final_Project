95ad7afdd9e6b2fd66881ccd24aafe2b710734dc
SyedaSanaHashmi <43295105+SyedaSanaHashmi@users.noreply.github.com>
2019-12-11 00:25:05 +0300

    Add files via upload

---
+++ USB Keyboard/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../usb_dev_keyboard_ccs.cmd 
+
+C_SRCS += \
+../startup_ccs.c \
+../usb_dev_keyboard.c \
+../usb_keyb_structs.c 
+
+C_DEPS += \
+./startup_ccs.d \
+./usb_dev_keyboard.d \
+./usb_keyb_structs.d 
+
+OBJS += \
+./startup_ccs.obj \
+./usb_dev_keyboard.obj \
+./usb_keyb_structs.obj 
+
+OBJS__QUOTED += \
+"startup_ccs.obj" \
+"usb_dev_keyboard.obj" \
+"usb_keyb_structs.obj" 
+
+C_DEPS__QUOTED += \
+"startup_ccs.d" \
+"usb_dev_keyboard.d" \
+"usb_keyb_structs.d" 
+
+C_SRCS__QUOTED += \
+"../startup_ccs.c" \
+"../usb_dev_keyboard.c" \
+"../usb_keyb_structs.c" 
+
+
